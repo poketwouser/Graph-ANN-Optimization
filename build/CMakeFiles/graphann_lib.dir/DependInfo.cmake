0a30fe2a0c2e765160fbbad43b23f95179958730
Kiran Kumar P <kirankumarp405@gmail.com>
2026-04-18 01:16:24 +0530

    Fun

---
+++ build/CMakeFiles/graphann_lib.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/kiran/Desktop/IITM/Semester 4/Algorithms for Data Science/End Semester Project/Graph-ANN-Optimization/src/distance.cpp" "CMakeFiles/graphann_lib.dir/src/distance.cpp.obj" "gcc" "CMakeFiles/graphann_lib.dir/src/distance.cpp.obj.d"
+  "C:/Users/kiran/Desktop/IITM/Semester 4/Algorithms for Data Science/End Semester Project/Graph-ANN-Optimization/src/io_utils.cpp" "CMakeFiles/graphann_lib.dir/src/io_utils.cpp.obj" "gcc" "CMakeFiles/graphann_lib.dir/src/io_utils.cpp.obj.d"
+  "C:/Users/kiran/Desktop/IITM/Semester 4/Algorithms for Data Science/End Semester Project/Graph-ANN-Optimization/src/vamana_index.cpp" "CMakeFiles/graphann_lib.dir/src/vamana_index.cpp.obj" "gcc" "CMakeFiles/graphann_lib.dir/src/vamana_index.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/graphann_lib.dir/src/distance.cpp.obj.d
@@ -0,0 +1,17 @@
+CMakeFiles/graphann_lib.dir/src/distance.cpp.obj: \
+ C:\Users\kiran\Desktop\IITM\Semester\ 4\Algorithms\ for\ Data\ Science\End\ Semester\ Project\Graph-ANN-Optimization\src\distance.cpp \
+ C:/Users/kiran/Desktop/IITM/SEMEST~4/ALGORI~1/ENDSEM~1/GRAPH-~1/include/distance.h \
+ C:/msys64/mingw64/include/c++/15.2.0/cstdint \
+ C:/msys64/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/c++config.h \
+ C:/msys64/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/os_defines.h \
+ C:/msys64/mingw64/include/c++/15.2.0/x86_64-w64-mingw32/bits/cpu_defines.h \
+ C:/msys64/mingw64/include/c++/15.2.0/pstl/pstl_config.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stdint.h \
+ C:/msys64/mingw64/include/stdint.h C:/msys64/mingw64/include/crtdefs.h \
+ C:/msys64/mingw64/include/corecrt.h C:/msys64/mingw64/include/_mingw.h \
+ C:/msys64/mingw64/include/_mingw_mac.h \
+ C:/msys64/mingw64/include/_mingw_secapi.h \
+ C:/msys64/mingw64/include/vadefs.h \
+ C:/msys64/mingw64/include/sdks/_mingw_ddk.h \
+ C:/msys64/mingw64/lib/gcc/x86_64-w64-mingw32/15.2.0/include/stddef.h \
+ C:/msys64/mingw64/include/stddef.h
